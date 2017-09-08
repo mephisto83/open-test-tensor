649f0e7617630c8a18b9a7f56bc1cffeb89d7934
Andrew Porter <mephisto83@hotmail.com>
2017-09-08 08:49:26 -0500

    updates

--- image-tools/elementextractiontest.js
+++ image-tools/elementextractiontest.js
@@ -1,46 +1,105 @@
 var activateAndPosition = require('./helpers/activateAndPosition');
 var fs = require('fs');
-var file = '/Users/andrewporter/Library/Developer/CoreSimulator/Devices/DED4C8F3-4715-4534-B01B-BCC75984644D/data/Containers/Bundle/Application/E4EDA6FB-DBB5-46D0-8A12-42ED8BBBFDCD/OpenTestTensorNativeBaseUIElements.app/open-test-tensor-current.json';
-
+var glob = require("glob")
+var file = '/Users/andrewporter/Library/Developer/CoreSimulator/Devices/DED4C8F3-4715-4534-B01B-BCC75984644D/data/Containers/Bundle/Application/7C52FC04-B811-4FC7-BE3E-DB793C52C314/OpenTestTensorNativeBaseUIElements.app/open-test-tensor-current.json';
+var documentpath = '../ui-examples/react-native/nativebase/';// '~/Documents/';
+var documentpathfolder = '../ui-examples/react-native/nativebase/images/';
 var robot = require("robotjs");
 robot.setKeyboardDelay(200);
+var btnOffset = 25;
+var btnOffset_y = 45;
+var lastItem = null;
 var screenshotdata = [];
+var image_count = 0;
+var window_header_height = 22;
 var windowPos = { x: 100, y: 100 };
-console.log(activateAndPosition);
-activateAndPosition.activateAndPosition().then((result) => {
-    return activateAndPosition.setPosition(100, 100).then(() => {
-        return result;
-    });
-}).then((boundaries) => {
-    return activateAndPosition.takeScreenShot(100, 100, boundaries[0], boundaries[1]);
-}).then(() => {
-    return new Promise(resolve => {
-        fs.readFile(file, 'utf8', function (err, data) {
-            if (err) {
-                return console.log(err);
-            }
-            screenshotdata.push(JSON.parse(data));
-            resolve(JSON.parse(data))
-        });
-    });
-}).then((action) => {
-    return activateAndPosition.takeScreenShot(
-        windowPos.x + action.pageX, 
-        windowPos.y + action.pageY + (action.frameHeight/2), 
-        action.frameWidth, 
-        action.frameHeight, `~/Documents/${action.name || 'unnamed.jpg'}`).then(() => {
-        return action;
-    });
-}).then((action) => {
-    console.log(action);
-    return robot.moveMouse(windowPos.x + action.pageX + (action.frameWidth / 2), windowPos.y + action.pageY + (action.frameHeight));
-}).then(() => {
+var total_images = 1;
+var pause = () => {
     return new Promise(resove => {
         setTimeout(() => {
             resove();
-        }, 4000)
+        }, 1000)
     })
-}).then(() => {
-    robot.mouseClick('left', 1);
-    console.log(screenshotdata);
-});
+};
+// console.log(activateAndPosition);
+var TestBoundaries = null;
+function generateImage() {
+    return activateAndPosition.activateAndPosition().then((result) => {
+        return activateAndPosition.setPosition(100, 100).then(() => {
+            return result;
+        });
+    }).then((boundaries) => {
+        TestBoundaries = boundaries;
+        return new Promise(resolve => {
+            fs.readFile(file, 'utf8', function (err, data) {
+                if (err) {
+                    return console.log(err);
+                }
+
+                lastItem = JSON.parse(data);
+                console.log(lastItem)
+                screenshotdata.push(lastItem);
+                resolve(lastItem)
+            });
+        });
+    }).then((action) => {
+        console.log(action);
+        robot.moveMouse(windowPos.x + btnOffset, windowPos.y + btnOffset_y)
+        return Promise.resolve().then(() => {
+            return action;
+        });
+    }).then((action) => {
+        var filename = `${lastItem.type}-${Date.now()}.jpg`;
+        lastItem.filename = filename;
+
+        return activateAndPosition.takeScreenShot(
+            windowPos.x + action.pageX,
+            windowPos.y + action.pageY + window_header_height,//
+            action.frameWidth,
+            action.frameHeight, `${documentpathfolder}${filename || 'unnamed.jpg'}`).then(() => {
+                return action;
+            });
+    }).then((action) => {
+        console.log(action);
+        return robot.moveMouse(windowPos.x + btnOffset, windowPos.y + btnOffset_y);
+    }).then(() => {
+        robot.mouseClick('left', 1);
+    }).then(pause).catch(e => {
+        console.log(e);
+    }).then(() => {
+        image_count++;
+        if (image_count < total_images) {
+            return generateImage();
+        }
+        else {
+            return Promise.resolve().then(() => {
+                var fileData = {
+                    screenshotdata,
+                    folder: 'images'
+                }
+                return new Promise(resolve => {
+                    fs.writeFile(`${documentpath}im-${Date.now()}.json`, JSON.stringify(fileData, null, 4), 'utf-8', function (err, data) {
+                        if (err) {
+                            console.log(err);
+                        }
+                        resolve();
+                    });
+                });
+            })
+        }
+    });
+}
+
+glob("/Users/andrewporter/Library/Developer/CoreSimulator/Devices/**/*/OpenTestTensorNativeBaseUIElements.app/open-test-tensor-current.json", function (er, files) {
+    // files is an array of filenames.
+    // If the `nonull` option is set, and nothing
+    // was found, then files is ["**/*.js"]
+    // er is an error object or null.
+    console.log(files);
+    console.log(er);
+    if (files.length) {
+        file = files[0];
+        generateImage();
+    }
+
+})
